b8d0c8021b9be2b58e12f0e213acf0519db37129
John Sweeney <email@sarno.dev>
2022-09-08 22:12:45 +0100

    feat: blog posts link to index, Fixes #8

---
+++ components/content/BackToBlogsIndex.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <hr>
+
+  Back to
+    <NuxtLink to="/posts">
+      all posts.
+    </NuxtLink>
+</div>
+</template>
+
+<script>
+export default {
+  name: "BackToBlogsIndex"
+}
+</script>
+
+<style scoped>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,7 @@
   <div class="max-w-2xl px-4 py-10 m-auto bg-white sm:px-8 sm:shadow dark:bg-primary-800 sm:rounded">
     <main class="prose prose-gray dark:prose-invert max-w-none">
       <slot />
+      <BackToBlogsIndex></BackToBlogsIndex>
     </main>
   </div>
 </template>
