349d90e0a56a70bf370850dc70d58f6b5a288dd5
John Sweeney <email@sarno.dev>
2022-09-08 21:50:22 +0100

    feat: blog posts index page! fixes #1

---
+++ components/content/BlogPosts.vue
@@ -0,0 +1,21 @@
+<script setup>
+const { data } = await useAsyncData(() => {
+  return queryContent('/posts/').find()
+})
+</script>
+
+<template>
+<div>
+  <div v-for="post in data" :key="post._id">
+    <h2>{{ post.title }}</h2>
+    <p>{{ post.description }}</p>
+    <NuxtLink :to="post._path">
+      Read more...
+    </NuxtLink>
+  </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
