02d7bbd5748876597ec9d9cdd0793058cf6f1b38
John Sweeney <email@sarno.dev>
2022-10-25 18:55:35 +0100

    chore: add creation timestamp to list of posts

--- components/content/BlogPosts.vue
+++ components/content/BlogPosts.vue
@@ -8,6 +8,7 @@ const { data } = await useAsyncData(() => {
 <div>
   <div v-for="post in data" :key="post._id">
     <h2>{{ post.title }}</h2>
+    <p><small>{{ post.createdAt }}</small></p>
     <p>{{ post.description }}</p>
     <NuxtLink :to="post._path">
       Read more...
